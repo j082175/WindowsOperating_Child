841745d7113299f0dff5399470ef0c95697b6de4
aa <aa@DESKTOP-SGCQREK>
2022-03-17 16:57:40 +0900

    초기 커밋.

---
+++ Notepad/Operation2.cpp
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+//#include <tchar.h>
+//#include <Windows.h>
+//
+//int _tmain(int argc, TCHAR argv[])
+//{
+//	::SetPriorityClass(::GetCurrentProcess(), HIGH_PRIORITY_CLASS);
+//
+//	while (true)
+//	{
+//		for (DWORD i = 0; i < 100000; i++)
+//		{
+//			for (DWORD i = 0; i < 10000; i++)
+//			{
+//
+//			}
+//		}
+//
+//		_fputts(_T("Operation2.exe\n"), stdout);
+//	}
+//}
